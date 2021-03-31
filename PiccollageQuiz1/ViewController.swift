deb3949d27db048f89bcd47220490213ebf38c89
Wei-Ting Ruan <ruan1031@gmail.com>
2021-04-01 07:09:47 +0800

    Code refinment

--- PiccollageQuiz1/ViewController.swift
+++ PiccollageQuiz1/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var processButton: UIButton!
     @IBOutlet weak var effectButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var gpuImageView: GPUImageView!
     
     private var firstVideo:AVAsset?
@@ -21,27 +22,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        effectButton.addTarget(self, action: #selector(self.effectButtonTapped(_:)), for: .touchUpInside)
+        effectButton.addTarget(self, action: #selector(effectButtonTapped(_:)), for: .touchUpInside)
         processButton.addTarget(self, action: #selector(processButtonTapped(_:)), for: .touchUpInside)
-//        guard let firstVideo = WTVideoReader(asset: AVAsset(url: Bundle.main.url(forResource: "IMG_2174", withExtension: "mp4")!)),
-//              let secondVideo = WTVideoReader(asset: AVAsset(url: Bundle.main.url(forResource: "IMG_2175", withExtension: "mp4")!)) else
-//        {
-//            return
-//        }
-//        
-//        var documentsPath = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask,true)[0]
-//        let fileName = "\(Int(Date().timeIntervalSince1970)).mov"
-//        print(fileName)
-//        documentsPath.append(fileName)
-//        let exportUrl = URL(fileURLWithPath: documentsPath)
-//        if(FileManager.default.fileExists(atPath: exportUrl.absoluteString)) {
-//            try? FileManager.default.removeItem(atPath: exportUrl.absoluteString )
-//        }
-//        
-//        if let movieWriter = GPUImageMovieWriter(movieURL: exportUrl, size: CGSize(width: 1080,height: 1920)),
-//           let transition = WTVideoTransitionFrameCache(firstVideo: firstVideo, secondVideo: secondVideo, animatedTime: 2.0, movieWriter: movieWriter, filePath: exportUrl) {
-//            self.transition = transition
-//        }
+        clearButton.addTarget(self, action: #selector(clearButtonTapped(sender:)), for: .touchUpInside)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -81,7 +64,7 @@ class ViewController: UIViewController {
             } else {
                 DispatchQueue.main.async {
                     sheet.dismiss(animated: true, completion: nil)
-                    self.showError()
+                    self.showError(message: "Select two video first")
                 }
             }
         }
@@ -94,7 +77,7 @@ class ViewController: UIViewController {
             } else {
                 DispatchQueue.main.async {
                     sheet.dismiss(animated: true, completion: nil)
-                    self.showError()
+                    self.showError(message: "Select two video first")
                 }
             }
         }
@@ -123,6 +106,7 @@ class ViewController: UIViewController {
             if let movieWriter = GPUImageMovieWriter(movieURL: exportUrl, size: CGSize(width: 1080,height: 1920)) {
                 movieWriter.encodingLiveVideo = false
                 let editor = WTVideoEditor(firstVideo: firstVideo!, secondVideo: secondVideo!, movieWriter: movieWriter, filePath: exportUrl, transitionEffect: transitionEffect!, finish: {
+                    self.transitionEffect = nil
                     DispatchQueue.main.async {
                         guard let player = GPUImageMovie(asset: AVAsset(url: exportUrl)) else {
                             return
@@ -136,13 +120,19 @@ class ViewController: UIViewController {
             }
         } else {
             DispatchQueue.main.async {
-                self.showError()
+                self.showError(message: "No selected video or effect two video")
             }
         }
     }
     
-    func showError() {
-        let alert = UIAlertController(title: "Error", message: "No video", preferredStyle: .alert)
+    @objc func clearButtonTapped(sender:UIButton) {
+        firstVideo = nil
+        secondVideo = nil
+        transitionEffect = nil
+    }
+    
+    func showError(message:String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(ok)
         self.present(alert, animated: true, completion: nil)
